7a49e937fb826e1744eb9203e67f6a711e3772a7
Devin McManus <devinmcmanus@DMC-Home-MBP.home>
2020-01-19 19:58:27 -0500

    added phara_people table

---
+++ models/reach/phara_people.sql
@@ -0,0 +1,12 @@
+{{
+    config(materialized='table'
+    tags=["reach"]
+    )
+}}
+
+SELECT * 
+FROM reach.bernie_people bp
+WHERE 
+  phara_support is not null
+  OR lower(reach_added_support) LIKE '%phara%'
+  or lower(notes) LIKE '%phara%'
